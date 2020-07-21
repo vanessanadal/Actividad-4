beda824e499268087fb89f9e011446020f11f9d1
david-monroy <djmonroy.17@est.ucab.edu.ve>
2020-07-20 23:33:15 -0400

    Mocha y Chai

--- shipthis-api/selenium_test.js
+++ shipthis-api/selenium_test.js
@@ -9,7 +9,7 @@ driver.get('http://www.google.com').then(function(){
 driver.findElement(webdriver.By.name('q')).sendKeys('webdriver\n').then(function(){
     driver.getTitle().then(function(title) {
       console.log(title)
-      if(title === 'webdriver - Buscar con Google') {
+      if(title === 'webdriver - Google Search') {
          console.log('Test passed');
       } else {
          console.log('Test failed');

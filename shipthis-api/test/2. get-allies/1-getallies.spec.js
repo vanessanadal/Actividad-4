aab17766b1c232a8b211807c32a50d06edc6d217
david-monroy <djmonroy.17@est.ucab.edu.ve>
2020-07-22 00:08:58 -0400

    Ruta 2 y Ruta 3

--- shipthis-api/test/2. get-allies/1-getallies.spec.js
+++ shipthis-api/test/2. get-allies/1-getallies.spec.js
@@ -2,27 +2,29 @@
 const { Builder, By, Key, until } = require('selenium-webdriver')
 const assert = require('assert')
 
-describe('ROUTE2.1', function() {
+describe('R2-get-allies', function() {
   this.timeout(30000)
   let driver
   let vars
   beforeEach(async function() {
-    driver = await new Builder().forBrowser('firefox').build()
+    driver = await new Builder().forBrowser('chrome').build()
     vars = {}
   })
   afterEach(async function() {
     await driver.quit();
   })
-  it('ROUTE2.1', async function() {
+  it('R2-get-allies', async function() {
     await driver.get("http://localhost:8085/")
-    await driver.manage().window().setRect(1552, 840)
-    await driver.findElement(By.id("login-email")).click()
+    await driver.manage().window().setRect(697, 728)
     await driver.findElement(By.id("login-email")).click()
     await driver.findElement(By.id("login-email")).sendKeys("admin@gmail.com")
     await driver.findElement(By.id("login-password")).click()
+    {
+      const element = await driver.findElement(By.css(".v-btn__content"))
+      await driver.actions({ bridge: true }).moveToElement(element).perform()
+    }
     await driver.findElement(By.id("login-password")).sendKeys("password")
     await driver.findElement(By.css(".v-btn__content")).click()
-    await driver.wait(until.elementLocated(By.id("sect5")),30000);
-    await driver.findElement(By.id("sect5")).click()
+    await driver.findElement(By.id("dash4")).click()
   })
 })
